d00c913b064950ed9e980937d585774407772387
Sophia Leão <sophialeaocardoso@hotmail.com>
2022-10-02 08:48:41 -0400

    adjustments to deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import About from "./components/About";
 import Home from "./components/Home";
 import Projects from "./components/pages/Projects";
 import { GlobalStyle } from "./global/GlobalStyle";
